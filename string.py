d524f5d964f2859425fb35fbe809a0cda3d55730
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-07 16:09:00 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,23 @@
+message = "sajin's birthday"
+print(message)
+print(message.find("n"))
+print(len(message))
+print(message.count("i"))
+
+name = 'hey im sajin"s friend'
+print(name)
+
+# method or function both are used to perform a specified task
+
+new_admission = "sajin"
+print(new_admission)
+print(new_admission.upper())
+print(new_admission.lower())  #invoking a method
+print(message.title())
+
+a = "happy birthday"
+b = "Aaron"
+print(a + " "+ b) # concatenate
+msg = message + name
+print(msg)
+print("hello \n world")     # \n - means escape sequence
